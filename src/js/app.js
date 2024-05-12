01dc425ab53634d4d3d5d7df1286a48df2eef380
Carine Lee <clee292@myseneca.ca>
2024-05-12 02:29:13 -0400

    Added dmg taken input and button to remove a row

--- src/js/app.js
+++ src/js/app.js
@@ -10,6 +10,8 @@ btn.addEventListener("click", function () {
     let ac_td = document.createElement("td");
     let currentHP_td = document.createElement("td");
     let maxHP_td = document.createElement("td");
+    let dmg_td = document.createElement("td");
+    let remove_td = document.createElement("td");
 
     let initInput = document.createElement("input");
     updateInputAttributes(initInput, "number", "init", "init");
@@ -35,6 +37,20 @@ btn.addEventListener("click", function () {
     updateInputAttributes(maxHPInput, "number", "max-hp", "max-hp");
     maxHP_td.appendChild(maxHPInput);
     row.appendChild(maxHP_td);
+
+    let dmgInput = document.createElement("input");
+    updateInputAttributes(dmgInput, "number", "dmg", "dmg");
+    dmg_td.appendChild(dmgInput);
+    row.appendChild(dmg_td);
+
+    //TODO: add click event on trash button to remove row
+    let removeBtn = document.createElement("button");
+    let trashIcon = document.createElement("i");
+    trashIcon.classList.add("fa-solid", "fa-trash-can");
+    removeBtn.appendChild(trashIcon);
+    remove_td.appendChild(removeBtn);
+    row.appendChild(remove_td);
+
 
     table.appendChild(row);
 });

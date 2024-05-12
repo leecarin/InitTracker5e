d70ef5331cccb2a387e82d6462728cf60f90ebd6
Carine Lee <clee292@myseneca.ca>
2024-05-12 13:58:57 -0400

    Remove btn functionality added, refactoring

--- src/js/app.js
+++ src/js/app.js
@@ -1,58 +1,65 @@
-let btn = document.querySelector("#add-row");
+let addBtn = document.querySelector("#add-row");
 let table = document.querySelector("#table-body");
+let rows = document.querySelectorAll(".input-row")
+
+let rmBtns = document.querySelectorAll(".rm-btn");
+for (let i = 0; i < rmBtns.length; i++) {
+    rmBtns[i].onclick = function () {
+        rows.forEach((elem) => deleteRow(elem));
+    }
+}
 
 //Add a row when + button clicked
-btn.addEventListener("click", function () {
+addBtn.addEventListener("click", function () {
     let row = document.createElement("tr");
+    row.classList.add("input-row");
 
-    let init_td = document.createElement("td");
-    let name_td = document.createElement("td");
-    let ac_td = document.createElement("td");
-    let currentHP_td = document.createElement("td");
-    let maxHP_td = document.createElement("td");
-    let dmg_td = document.createElement("td");
-    let remove_td = document.createElement("td");
+    let init_td = row.insertCell();
+    let name_td = row.insertCell();
+    let ac_td = row.insertCell();
+    let currentHP_td = row.insertCell();
+    let maxHP_td = row.insertCell();
+    let dmg_td = row.insertCell();
+    let remove_td = row.insertCell();
 
     let initInput = document.createElement("input");
     updateInputAttributes(initInput, "number", "init", "init");
     init_td.appendChild(initInput);
-    row.appendChild(init_td);
 
     let nameInput = document.createElement("input");
     updateInputAttributes(nameInput, "text", "name", "name");
     name_td.appendChild(nameInput);
-    row.appendChild(name_td);
 
     let acInput = document.createElement("input");
     updateInputAttributes(acInput, "number", "armour-class", "armour-class");
     ac_td.appendChild(acInput);
-    row.appendChild(ac_td);
 
     let currentHPInput = document.createElement("input");
     updateInputAttributes(currentHPInput, "number", "current-hp", "current-hp");
     currentHP_td.appendChild(currentHPInput);
-    row.appendChild(currentHP_td);
 
     let maxHPInput = document.createElement("input");
     updateInputAttributes(maxHPInput, "number", "max-hp", "max-hp");
     maxHP_td.appendChild(maxHPInput);
-    row.appendChild(maxHP_td);
 
     let dmgInput = document.createElement("input");
     updateInputAttributes(dmgInput, "number", "dmg", "dmg");
     dmg_td.appendChild(dmgInput);
-    row.appendChild(dmg_td);
 
-    //TODO: add click event on trash button to remove row
     let removeBtn = document.createElement("button");
+    removeBtn.classList.add("rm-btn");
     let trashIcon = document.createElement("i");
     trashIcon.classList.add("fa-solid", "fa-trash-can");
     removeBtn.appendChild(trashIcon);
     remove_td.appendChild(removeBtn);
-    row.appendChild(remove_td);
-
 
     table.appendChild(row);
+
+    //Remove row if trash can button clicked
+    removeBtn.onclick = function () {
+        deleteRow(row);
+    }
+
 });
 
 function updateInputAttributes(element, type, classAttr, nameAttr, min = 0) {
@@ -62,6 +69,12 @@ function updateInputAttributes(element, type, classAttr, nameAttr, min = 0) {
     element.type = type;
     element.classList.add(classAttr);
     element.name = nameAttr;
+}
+
+function deleteRow(tr) {
+    console.log(`Row index is ${tr.rowIndex}`);
+    let idx = tr.rowIndex;
+    table.deleteRow(idx - 1);
 }
 
 //TODO: Sort table rows by initiative order
